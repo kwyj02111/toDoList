380493650df5ac50ca447ede17bcfd509d11e499
KWON\Administrator <kwyj0211@gmail.com>
2018-09-10 16:23:31 +0900

    [add] Apply Login CSS

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Login from './Login';
+import '../assets/css/home.css';
 
 const Home = ({loginState, loginClick}) => {
 
     return (
-        <div>
+        <div className="home-container">
             <Login
                 loginState = {loginState}
                 loginClick = {loginClick}
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/css/login.css';
 
 const Login = ({ loginState, loginClick }) => {
 
@@ -7,8 +8,9 @@ const Login = ({ loginState, loginClick }) => {
     let inputPwd = '';
 
     return (
-        <div>
+        <div className="login-container">
             <form
+                className="login-input-area"
                 onSubmit={ e => {
                     e.preventDefault()
 
@@ -19,25 +21,32 @@ const Login = ({ loginState, loginClick }) => {
                     loginClick(inputId.value, inputPwd.value);
                 }}
             >
-                <input
-                    className="login-id-input"
-                    type="text"
-                    placeholder="id"
-                    ref = {node => inputId = node}
-                />
+                <div className="login-input-box">
+                    <i className="login-input-icon material-icons">&#xE853;</i>
+                    <input
+                        className="login-id-input"
+                        type="text"
+                        placeholder="id"
+                        ref = {node => inputId = node}
+                    />
+                </div>
 
-                <input
-                    className="login-pwd-input"
-                    type="password"
-                    placeholder="password"
-                    ref = {node => inputPwd = node}
-                />
+                <div className="login-input-box">
+                    <i className="login-input-icon material-icons">&#xE0DA;</i>
+                    <input
+                        className="login-pwd-input"
+                        type="password"
+                        placeholder="password"
+                        ref = {node => inputPwd = node}
+                    />
+                </div>
+
                 <button
                     className="login-btn"
                     onClick={loginClick}
                     type="submit"
                 >
-                    Login!!!!!!!!!!!
+                    Login!
                 </button>
             </form>
         </div>
@@ -45,5 +54,8 @@ const Login = ({ loginState, loginClick }) => {
     );
 };
 
+Login.propTypes = {
+    loginClick: PropTypes.func.isRequired,
+}
 
 export default Login;
