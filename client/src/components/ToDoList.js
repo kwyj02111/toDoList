a70aab39d3665fd59285b9f2d261d61b5e9772c7
KWON\Administrator <kwyj0211@gmail.com>
2018-10-16 18:38:33 +0900

    [add] Add Todolist Contents

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import * as axiosService from '../services/axios/axios';
 import * as userInfoService from '../services/userInfo/userInfo';
 import { getListURL } from '../services/axios/axiosUrl';
+import '../assets/css/toDoList.css';
 
 class ToDoList extends Component {
 
@@ -51,12 +52,45 @@ class ToDoList extends Component {
 
     render() {
         return (
-            <div>
-            TodoList!!!
+            <div className="todolist-container">
+                <div className="todolist-area">
+                    <div className="todolist-input-box">
+                        <input
+                            className="todolist-input"
+                            type="text"
+                            placeholder="+ add ToDoList"
+                        />
+
+                        <button className="todolist-add-btn">
+                            <i className="todolist-add-btn-icon material-icons">playlist_add</i>
+                        </button>
+                    </div>
+
+                    <ul className="todolist-item-box">
+                        {this.state.list.map(item =>
+                            <li key={item.NO}
+                                className="todolist-item"
+                            >
+                                <button className="todolist-item-check-btn">
+                                    {item.IS_COMPLETE > 0 ?
+                                        <i className="todolist-item-check-icon material-icons">done</i> : ''
+                                    }
+                                </button>
+
+                                <div className={item.IS_COMPLETE > 0 ? 'todolist-item-content done' : 'todolist-item-content'}>
+                                    {item.CONTENT}
+                                </div>
+
+                                <button className="todolist-item-menu-btn">
+                                    <i className="todolist-item-menu-icon material-icons">more_vert</i>
+                                </button>
+                            </li>
+                        )}
+                    </ul>
+                </div>
             </div>
         );
     }
-
 };
 
 export default ToDoList;
